4429f545c6a668e329f1016346caa6a01efdb65a
starkvijayannaa <145591255+starkvijayannaa@users.noreply.github.com>
2023-10-21 15:19:58 +0530

    Update torrent_search.py

--- bot/modules/torrent_search.py
+++ bot/modules/torrent_search.py
@@ -281,6 +281,6 @@ async def torrentSearchUpdate(_, query):
 
 
 bot.add_handler(MessageHandler(torrentSearch, filters=command(
-    BotCommands.SearchCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+    BotCommands.SearchCommand)))
 bot.add_handler(CallbackQueryHandler(
     torrentSearchUpdate, filters=regex("^torser")))
